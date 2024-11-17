bf8bdeff9497cd3072784e4a8d9e15a357de85b0
icebing <xu2237803016@outlook.com>
2024-11-17 12:26:41 +0800

    chore: 同步 debug 状态

--- internal/debug.go
+++ internal/debug.go
@@ -5,7 +5,7 @@ import "os"
 var DebugEnable = false
 
 func init() {
-	if os.Getenv("DEBUG") != "" {
+	if os.Getenv("DEBUG_ENABLED") == "true" {
 		DebugEnable = true
 	} else {
 		DebugEnable = false
